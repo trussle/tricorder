ec2370fd8080b90c88906d02ac22ff2df5df0afb
Tim Reynolds <tim@timothyreynolds.co.uk>
2018-03-16 12:21:03 +0100

    Added d.ts to tricorder (#7)
    * Added d.ts to tricorder
    
    * Updated client type in d.ts file

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+import * as promClient from "prom-client";
+import * as express from "express";
+
+export const client: typeof promClient; 
+export declare function instrument (server: express.Express): void;
