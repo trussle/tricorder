2c198de3f61f53fc9b4b2c59943ea5445a14bf65
Tim Reynolds <tim@timothyreynolds.co.uk>
2018-03-16 11:12:02 +0000

    Updated client type in d.ts file

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,5 @@
 import * as promClient from "prom-client";
 import * as express from "express";
 
-// TODO: this shoudl be all of promClient
-// not sure how to get that to work in a declaration file
-export const client: any; 
+export const client: typeof promClient; 
 export declare function instrument (server: express.Express): void;
